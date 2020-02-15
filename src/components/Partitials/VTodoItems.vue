16ec75464bd3c84d222741709b3ed237311f017e
jc_dstepas <dstepas@justcoded.co>
2020-02-16 00:53:37 +0200

    todo app

---
+++ src/components/Partitials/VTodoItems.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <input
+      type="checkbox"
+    >
+    <span>
+      Bla
+    </span>
+    <button>
+      Mark as done
+    </button>
+    <button>
+      Remove
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+
+};
+</script>
